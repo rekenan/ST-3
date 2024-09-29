63fc8285f31c7d63b01d63e3a0ab5d34d5bf9e28
Мамедов Кенан Эльчин Оглы <k.mamedov@kotologika.ru>
2024-09-29 16:54:39 +0300

    fix

--- src/TimedDoor.cpp
+++ src/TimedDoor.cpp
@@ -1,5 +1,5 @@
 // Copyright 2024 MamedovKenan
-#include "TimedDoor.h"
+#include "../include/TimedDoor.h"
 #include <stdexcept>
 #include <thread>
 #include <chrono>
@@ -7,43 +7,46 @@
 DoorTimerAdapter::DoorTimerAdapter(TimedDoor& d) : door(d) {}
 
 void DoorTimerAdapter::Timeout() {
-  if (door.isDoorOpened()) {
-    door.throwState();
-  }
+    if (door.isDoorOpened()) {
+        door.throwState();
+    }
 }
 
 TimedDoor::TimedDoor(int timeout) : iTimeout(timeout), isOpened(false) {
-  adapter = new DoorTimerAdapter(*this);
+    adapter = new DoorTimerAdapter(*this);
 }
 
 bool TimedDoor::isDoorOpened() {
-  return isOpened;
+    return isOpened;
 }
 
 void TimedDoor::unlock() {
-  isOpened = true;
+    isOpened = true;
+    Timer timer;
+    timer.tregister(iTimeout, adapter);
 }
 
 void TimedDoor::lock() {
-  isOpened = false;
+    isOpened = false;
 }
 
 int TimedDoor::getTimeOut() const {
-  return iTimeout;
+    return iTimeout;
 }
 
 void TimedDoor::throwState() {
-  throw std::runtime_error("Door is open for too long!");
+    throw std::runtime_error("Door is open for too long!");
 }
 
 void Timer::tregister(int seconds, TimerClient* client) {
-  this->client = client;
-  std::thread([this, seconds]() {
-    sleep(seconds);
-    client->Timeout();
-  }).detach();
+    this->client = client;
+
+    std::thread([this, seconds]() {
+        sleep(seconds);
+        client->Timeout();
+    }).detach();
 }
 
 void Timer::sleep(int seconds) {
-  std::this_thread::sleep_for(std::chrono::seconds(seconds));
+    std::this_thread::sleep_for(std::chrono::seconds(seconds));
 }
